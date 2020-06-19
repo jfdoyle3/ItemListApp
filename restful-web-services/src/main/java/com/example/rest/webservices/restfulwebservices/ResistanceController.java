c443647c9955ba7b8b905780fa44cab7b3ab0661
Jim Doyle <indellibleink@gmail.com>
2020-06-18 20:08:44 -0400

    Created 2 ways of making GET routes.

---
+++ restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/ResistanceController.java
@@ -0,0 +1,29 @@
+package com.example.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller
+@RestController
+public class ResistanceController {
+	 
+		// GET
+		// URI - /resistance
+		// Both Method 1 and 2 return a message to the page.
+		// method - "Resistance is Futile!"
+	    // method 1: @RequestMapping(method=RequestMethod.GET, path="/resistance")
+		//method 2: shorter, results the same output as method 1.
+		@GetMapping(path="/resistance")
+		public String resistance() {
+			return "Resistance is Futile!";
+		}
+		
+		
+		// this returns a JSON response to the page.
+		// resistance - bean
+		@GetMapping(path="/resistance-bean")
+		public resistanceBean resistanceBean() {
+			return new resistanceBean("Resistance is Futile!");
+		}
+		
+}
---
+++ restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/resistanceBean.java
@@ -0,0 +1,24 @@
+package com.example.rest.webservices.restfulwebservices;
+
+public class resistanceBean {
+
+	private String message;
+
+	public resistanceBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "resistanceBean [message=" + message + "]";
+	}
+
+}
