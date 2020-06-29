b2ad08c9faa3370e87b6977b2e2bc5a5617a6a90
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2020-06-29 16:47:13 -0400

    repaired broken react app

--- frontend/itemlist-app/src/components/examples/SecondComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-// Another way to make a component.
-// preferred way is First Component
-export default class Items extends Component {
-  render() {
-    return <div className="Items">Second Component - named: Class Items</div>;
-  }
-}
--- frontend/itemlist-app/src/components/examples/ThirdComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// function can be used as Components
-// they can be exported this way, preferd method is First component example
-// function components can't have state funcationality.
-// Must be a class to use states.
-export default function FunctionComponent() {
-  return <div className="FunctionComponent">Function Component</div>;
-}
